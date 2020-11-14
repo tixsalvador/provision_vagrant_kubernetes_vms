52c322d089128caf8eca0c2219d721eb64c05154
tix salvador <tix.salvador@gmail.com>
2020-11-14 14:44:21 -0500

    fix

--- worker.sh
+++ worker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Adding to cluster"
 sshpass  -p vagrant  ssh  -o "StrictHostKeyChecking no" vagrant@10.10.10.100 'sudo kubeadm token create --print-join-command' > /root/join
 chmod +x /root/join
-/usr/bin/sh /root/join
+/usr/bin/sh /root/join > /dev/null 2>&1
