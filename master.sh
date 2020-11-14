4b5e2171737388cafb29390e009a62967e2b80b2
t|x Salvador <tix.salvador@gmail.com>
2020-11-14 17:23:05 -0500

    Update master.sh

--- master.sh
+++ master.sh
@@ -5,7 +5,7 @@ POD_NETWORK=10.244.0.0/16
 
 echo "Building cluster"
 kubeadm init --pod-network-cidr $POD_NETWORK --apiserver-advertise-address $IP > /dev/null 2>&1
-mkdir -p $HOME/.kube
-/bin/cp -if /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/tixsalvador/ansible_vagrant/master/files/kube-flannel.yml > /dev/null 2>&1
+sudo -u vagrant mkdir -p $HOME/.kube
+sudo -u vagrant /bin/cp -if /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo -u vagrant chown $(id -u):$(id -g) $HOME/.kube/config
+sudo -u vagrant kubectl apply -f https://raw.githubusercontent.com/tixsalvador/ansible_vagrant/master/files/kube-flannel.yml > /dev/null 2>&1
