57da808c9fb7c24ac1717b4b5bbd13c9fed97e14
tix salvador <tix.salvador@gmail.com>
2020-11-14 14:38:13 -0500

    fix

--- worker.sh
+++ worker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 sshpass  -p vagrant  ssh  -o "StrictHostKeyChecking no" vagrant@10.10.10.100 'sudo kubeadm token create --print-join-command' > /root/join
+chmod +x /root/join
+/usr/bin/sh /root/join
