acbd682c14af553063bee2911c77a096870fafc4
tix salvador <tix.salvador@gmail.com>
2020-11-14 17:42:49 -0500

    fix vagrant user running kubectl

--- worker.sh
+++ worker.sh
@@ -6,4 +6,3 @@ echo "Adding to cluster"
 sshpass  -p vagrant  ssh  -o "StrictHostKeyChecking no" vagrant@$MASTER 'sudo kubeadm token create --print-join-command' > /root/join
 chmod +x /root/join
 /usr/bin/sh /root/join > /dev/null 2>&1
-echo "su - vagrant && mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config" 
