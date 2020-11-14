13baee761c5470b69e3d49ad934be3b3f68e1862
tix salvador <tix.salvador@gmail.com>
2020-11-14 13:25:59 -0500

    fix script

--- master.sh
+++ master.sh
@@ -3,5 +3,6 @@
 IP=`hostname -I | awk '{print $2}'`
 POD_NETWORK=10.244.0.0/16
 
+echo "Building cluster"
 kubeadm init --pod-network-cidr $POD_NETWORK --apiserver-advertise-address $IP
 kubectl apply -f https://raw.githubusercontent.com/tixsalvador/ansible_vagrant/master/files/kube-flannel.yml
