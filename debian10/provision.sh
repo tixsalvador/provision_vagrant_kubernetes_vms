4287bf3e7afe80bcfb96eafb3fe65d50f327cb32
tix <tix.salvador@gmail.com>
2021-06-16 19:32:16 -0400

    add helm install

--- debian10/provision.sh
+++ debian10/provision.sh
@@ -29,9 +29,11 @@ sysctl --system
 # Add additional repos
 echo "Add repos"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - > /dev/null 2>&1
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -  > /dev/null 2>&1
 echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null 2>&1
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list > /dev/null 2>&1
 apt-get update -y
 
 # Install containerd
@@ -42,7 +44,7 @@ systemctl restart containerd
 
 # Install kubeadm, kubectl, and kubelet
 echo "Install kubeadm, kubectl, and kubelet"
-apt-get install -y apt-transport-https ca-certificates curl
+apt-get install -y apt-transport-https ca-certificates curl helm
 apt-get install -y kubectl kubeadm kubelet
 IP=`hostname -I | awk '{print $2}'`
 sed '/Environment=\"KUBELET_CONFIG_ARGS=/a Environment=\"KUBELET_EXTRA_ARGS=--node-ip='$IP'\"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf -i
